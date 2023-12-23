c2ce4ee0d37e05396384c91a6d7dbb483b26bad8
Paul Cameron <pcameron5@gmail.com>
2023-12-23 07:29:03 -0500

    PS-001: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { db } from './firebase-config';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState([]);
+  const [age, setAge] = useState([]);
+  const UsersCollectionRef = collection(db, "users")
+
+  const CreateUser = async () => {
+    await addDoc(UsersCollectionRef, { Name: name, age: age })
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    const getUsersData = async () => {
+      const data = await getDocs(UsersCollectionRef)
+      setUsers(data.docs.map((elem) => ({ ...elem.data(), id: elem.id })))
+    }
+
+    getUsersData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-white'>
+      <h1 className=' w-screen text-center mt-8 text-4xl font-bold'>React with FireBase</h1>
+      <p className='w-screen text-center mt-5'>Fillin the Details to Upload Data to the DataBase</p>
+      <div className='text-center mt-16'>
+        <span>Enter your Name : </span>
+        <input className='mx-4 text-black' type="text" placeholder='Name' onChange={(event) => { setName(event.target.value) }} />
+        <span>Enter your Age : </span>
+        <input className='mx-4 text-black' type="text" placeholder='Age' onChange={(event) => { setAge(event.target.value) }} />
+        <br />
+        <button onClick={CreateUser} className='bg-slate-700 m-4 p-2 w-20 rounded-md'>Upload</button>
+      </div>
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7MHemqC-gnR11EeR8KXwXIQJnrOYRHUk",
+  authDomain: "pawnspace-e65a5.firebaseapp.com",
+  projectId: "pawnspace-e65a5",
+  storageBucket: "pawnspace-e65a5.appspot.com",
+  messagingSenderId: "117732935602",
+  appId: "1:117732935602:web:a1e40fb33a7d8d413b46ab",
+  measurementId: "G-GWCRJHB04R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
